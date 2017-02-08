535df3bad9dab2227ecd64539e7df6eb4733942c
Governor <elimushwari@gmail.com>
2017-02-08 06:52:56 +0300

    More corrections

--- Primes.py
+++ Primes.py
@@ -1,28 +1,25 @@
-Def output_prime_numbers(start, upTo):
+def output_prime_numbers(start, up_to):
 	prime_list = []
+	
 	#checks whether start is greater than upTo
-	If start > upTo:
-		return "This thing is wrong”
+	if start > up_to:
+		raise ValueError("This is wrong data")
+	
 	#checks whether the data type is correct
-	elif type(start) != int or type(upTo) != int:
-		Raise TypeError(“Wrong Type of data”)
-	#returns an empty list in case there’s no prime number
-	elif start  < 2 and upto < 2:
-`		return prime_list
-	#Ensures that we start counting up from 2
-Elif start < 2:
-		Return out_put_primenumbers(2, upTo)
-	Else:
-		#to save the time and resources, we’ll only work with Odd numbers, except “2”
-prime_list = [2]
-		For num in range(3, upTo, 2)
-			if is_prime(num):
-				prime_list.add(num)
-		return prime_list
+	elif type(start) != int or type(up_to) != int:
+		raise TypeError("Wrong Type of data")
 
-Def is_prime(num):
-	#only working with odd numbers starting from 3
-	For I in range(3, num, 2):
-		If num % I == 0:
-			Return False
-	Return True
+	#returns an empty list in case there’s no prime number
+	elif start < 2 and up_to < 2:
+	  return prime_list
+#Ensures that we start counting up from 2
+	elif start < 2:
+		return output_prime_numbers(2, up_to)
+	else:
+		
+		#to save the time and resources, we’ll only work with Odd numbers, except “2”
+		prime_list = [2]
+		for num in range(3, up_to + 1, 2):
+			if is_prime(num, prime_list):
+				prime_list.append(num)
+		return prime_list
