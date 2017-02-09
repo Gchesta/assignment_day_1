bdc5c4bb0449fe8214ad04dff79b40a5f5744aeb
Governor <elimushwari@gmail.com>
2017-02-09 23:38:01 +0300

    Add two files after recommended corrections

--- Primes.py
+++ Primes.py
@@ -3,27 +3,37 @@ def output_prime_numbers(start, up_to):
 	
 	#checks whether start is greater than upTo
 	if start > up_to:
-		raise ValueError("This is wrong data")
+		return "This is wrong data"
 	
 	#checks whether the data type is correct
 	elif type(start) != int or type(up_to) != int:
-		raise TypeError("Wrong Type of data")
+		return "Wrong Type of Data"
 
 	#returns an empty list in case there's no prime number
 	elif start < 2 and up_to < 2:
 	  return prime_list
 	#Ensures that we start counting up from 2
-	elif start < 2:
-		return output_prime_numbers(2, up_to)
-	else:
-		
-		#to save the time and resources, we'll only work with Odd numbers, except "2"
+	elif start <= 2:
 		prime_list = [2]
 		for num in range(3, up_to + 1, 2):
 			if is_prime(num, prime_list):
 				prime_list.append(num)
 		return prime_list
 
+	elif start % 2 == 0:
+		#to save the time and resources, we'll only work with Odd numbers, except "2"
+		odd_start = start + 1
+		for num in range(odd_start, up_to + 1, 2):
+			if is_prime(num, prime_list):
+				prime_list.append(num)
+		return prime_list
+
+	else:
+		for num in range(start, up_to + 1, 2):
+			if is_prime(num, prime_list):
+				prime_list.append(num)
+		return prime_list
+		
 def is_prime(num, prime_list):
 	"""This function is used to determine whether a number is prime or 
 	not. To ensure that the code is as efficient as 
@@ -34,5 +44,7 @@ def is_prime(num, prime_list):
 		if num % prime_num == 0:
 			return False
 	return True
+
+print(output_prime_numbers(0, 100))
 
 	
--- Tests.py
+++ Tests.py
@@ -8,16 +8,16 @@ class OutputPrimeTests(unittest.TestCase):
 	def test_minus_ten_to_ten(self):
 		self.assertEqual(output_prime_numbers(-10, 10), [2, 3, 5, 7])
 	def test_ten_to_0(self):
-		self.assertEqual(output_prime_numbers(10, 0), "This thing is wrong")
+		self.assertEqual(output_prime_numbers(10, 0), "This is wrong data")
 	def test_twenty_twenty(self):
 		self.assertEqual(output_prime_numbers(20, 20), [])
-	def tes_typeerror(self):
+	def test_typeerror(self):
 	        with self.assertRaises(TypeError):
-		        raiseError("Wrong Type of Data")
+		        raise TypeError("Wrong Type of Data")
 	def works_with_zero(self):
 		self.assertEqual(output_prime_numbers(0, 0), [])	
 	def test_large_values(self):
-		self.asertEquals(output_prime_numbers(0, 100), [2, 3, 5, 7, 11, 13, 15, 17, 19, 23,            29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+		self.assertEqual(output_prime_numbers(0, 100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
 	def test_negatives_only(self):
 		self.assertEqual(output_prime_numbers(-10, -1), [])
 	def test_wrong_type(self):
@@ -25,5 +25,7 @@ class OutputPrimeTests(unittest.TestCase):
 	def no_float_variables(self):
 		self.assertEqual(output_prime_numbers(2.0, 6.4), "Wrong Type of Data" )
 
+if __name__ == "__main__":
+    unittest.main()
 
 
